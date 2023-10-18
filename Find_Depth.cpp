eb60d48d2520a01de70a2e93e0114a0b896a5f31
Darshan Kumar T <darshan_kumar_t@outlook.com>
2023-10-18 12:06:14 +0530

    Create Find_Depth.cpp

---
+++ Find_Depth.cpp
@@ -0,0 +1,9 @@
+int FindDepth(Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int leftDepth = FindDepth(root->left);
+    int rightDepth = FindDepth(root->right);
+
+    return (leftDepth > rightDepth) ? (leftDepth + 1) : (rightDepth + 1);
+}
