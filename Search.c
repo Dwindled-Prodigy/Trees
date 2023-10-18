63c6c78396f31645c4a69ed04315cc51520b3f28
Darshan Kumar T <darshan_kumar_t@outlook.com>
2023-10-18 11:58:27 +0530

    Create Search.c

---
+++ Search.c
@@ -0,0 +1,11 @@
+bool Search(struct Node* root, int target) {
+    if (root == NULL)
+        return false;
+
+    if (root->data == target)
+        return true;
+    else if (target < root->data)
+        return Search(root->left, target);
+    else
+        return Search(root->right, target);
+}
