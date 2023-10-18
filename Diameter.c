0a987a790720e2b0bed85e4f0f54b76f01e9fb86
Darshan Kumar T <darshan_kumar_t@outlook.com>
2023-10-18 12:09:37 +0530

    Create Diameter.c

---
+++ Diameter.c
@@ -0,0 +1,12 @@
+int FindDiameter(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int leftHeight = FindHeight(root->left);
+    int rightHeight = FindHeight(root->right);
+
+    int leftDiameter = FindDiameter(root->left);
+    int rightDiameter = FindDiameter(root->right);
+
+    return max(max(leftDiameter, rightDiameter), leftHeight + rightHeight + 1);
+}
